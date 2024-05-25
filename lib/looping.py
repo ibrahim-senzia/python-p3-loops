d0015545b69a8ab6cb31f5c61f7fedf56f2acd79
Ibrahim Senzia <ibrahimsenzia@gmail.com>
2024-05-25 10:17:36 +0300

    loops lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,31 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 11
+    while i > 0:
+            i -= 1
+            print(i)
+    if i == 0:
+         print('Happy New Year!')
+happy_new_year()
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+     int_list = [num ** 2 for num in int_list]
+     return int_list
+
+square_integers([1,2,4])
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,101):
+         if i % 3 == 0 and i % 5 == 0:
+              print('FizzBuzz')
+         elif i % 5 == 0:
+              print('Buzz')
+         elif i % 3 == 0:
+              print('Fizz')
+         else:
+              print(i)
+fizzbuzz()
